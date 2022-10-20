2cd1bffb5f40147470d277e899119c69a756604d
Windson Viana <windson@virtual.ufc.br>
2022-10-20 17:58:50 -0300

    Alterando a gravação e leitura do Shared Preferences e o Histórico

--- app/src/main/java/ufc/smd/esqueleto_placar/PreviousGamesActivity.kt
+++ app/src/main/java/ufc/smd/esqueleto_placar/PreviousGamesActivity.kt
@@ -27,9 +27,7 @@ class PreviousGamesActivity : AppCompatActivity() {
         recyclerview.layoutManager = LinearLayoutManager(this)
 
         // O ArrayList de Placares
-        val data = ArrayList<Placar>()
-
-        readPLacarData()
+        val data = readPLacarDataSharedPreferences()
        // val date = Calendar.getInstance().time
        // var dateTimeFormat = SimpleDateFormat("dd/MM/yy HH:mm:ss", Locale.getDefault())
        // val data_hora = dateTimeFormat.format(date)
@@ -37,12 +35,12 @@ class PreviousGamesActivity : AppCompatActivity() {
 
 
         //Criando 10 Placares
-        for (i in 1..10) {
-            val date = Calendar.getInstance().time
-            var dateTimeFormat = SimpleDateFormat("dd/MM/yy HH:mm:ss", Locale.getDefault())
-            val data_hora = dateTimeFormat.format(date)
-            data.add(Placar("Jogo "+i,""+i+"x"+i," O jogo foi 4x4 em "+data_hora+"h",true))
-        }
+     //   for (i in 1..10) {
+       //     val date = Calendar.getInstance().time
+         //   var dateTimeFormat = SimpleDateFormat("dd/MM/yy HH:mm:ss", Locale.getDefault())
+           // val data_hora = dateTimeFormat.format(date)
+            //data.add(Placar("Jogo "+i,""+i+"x"+i," O jogo foi 4x4 em "+data_hora+"h",true))
+        //}
 
         // ArrayList enviado ao Adapter
         val adapter = CustomAdapter(data)
@@ -50,6 +48,33 @@ class PreviousGamesActivity : AppCompatActivity() {
         // Setando o Adapter no Recyclerview
         recyclerview.adapter = adapter
 
+    }
+    fun readPLacarDataSharedPreferences():ArrayList<Placar> {
+        Log.v ("PDM", "Lendo o Shared Preferences")
+        val data = ArrayList<Placar> ()
+        val sharedFileName = "PreviousGames"
+        var aux: String
+        var sp: SharedPreferences = getSharedPreferences(sharedFileName, Context.MODE_PRIVATE)
+        if(sp!=null) {
+            var numMatches= sp.getInt("numberMatch", 0)
+            Log.v("PDM", "numMatchs:"+ numMatches)
+            for (i in 1.. numMatches){
+                aux= sp.getString("match"+i,"vazio")!!
+                if(!aux.equals("vazio")){
+
+                    var bis:ByteArrayInputStream
+                    bis = ByteArrayInputStream(aux.toByteArray(Charsets.ISO_8859_1))
+                    var obi:ObjectInputStream
+                    obi = ObjectInputStream(bis)
+
+                    var placar: Placar = obi.readObject() as Placar
+                    data.add(placar)
+                    //Log.v("PDM", "match"+i+" :"+aux)
+                    Log.v("PDM", "Placar: "+placar.nome_partida +" Res:"+placar.resultadoLongo)
+                }
+            }
+        }
+        return data
     }
     fun readPLacarData(){
         Log.v ("PDM", "Lendo o Shared Preferences")
